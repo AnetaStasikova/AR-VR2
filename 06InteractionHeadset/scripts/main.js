bc7d1a6e57b4fd2beb1417f3be52d05633d93bdc
AnetaStasikova <stasikovaa@gmail.com>
2020-05-13 21:17:19 +0200

    Update main.js

--- 06InteractionHeadset/scripts/main.js
+++ 06InteractionHeadset/scripts/main.js
@@ -33,7 +33,7 @@ var growspeed = 1.2;
  	myOtherBox.object3D.scale.x *= growspeed;
  	myOtherBox.object3D.scale.y *= growspeed;
  	myOtherBox.object3D.scale.z *= growspeed;
- 	//console.log(myOtherBox.object3D.scale);
+ 	console.log(myOtherBox.object3D.scale);
  }
 
  myOtherBox.addEventListener('click', function(){ // uses a fuse
